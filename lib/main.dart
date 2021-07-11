ccec85269c4e586bb1b261d63ffd02340a5609d1
MinhTriNguyen <minhtri.minhzzxzx@gmail.com>
2021-07-12 04:53:15 +0700

    Implement Json Parse and Delete List

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:loading_animations/loading_animations.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'dart:convert';
 
 void main() => runApp(new MyApp());
@@ -38,22 +36,11 @@ class _MyHomePageState extends State<MyHomePage> {
   void _getUsers() async {
     var _data = await getJson();
 
-    var jsonData = json.decode(_data);
+    var userList = json.decode(_data) as List;
 
-    List<User> users = [];
+    List<User> users = userList.map((user) => User.fromJson(user)).toList();
 
-    for (var u in jsonData) {
-      User user = User(
-        u["index"],
-        u["name"],
-        u["album"],
-        u["picture"],
-      );
-
-      users.add(user);
-    }
-
-    await Future.delayed(const Duration(seconds: 2));
+    await Future.delayed(const Duration(seconds: 3));
 
     setState(() {
       data = users;
@@ -70,70 +57,88 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    if (data == null) {
-      return new Scaffold(
-        backgroundColor: Colors.white,
-        body: Container(
-          child: Center(
-            child: LoadingBouncingGrid.square(
-              backgroundColor: Colors.red,
-              borderColor: Colors.red,
-              duration: const Duration(seconds: 3),
-            ),
-          ),
-        ),
-      );
-    } else {
-      return new Scaffold(
-        appBar: new AppBar(
-          title: new Text(widget.title),
-        ),
-        body: Container(
-          child: ListView.builder(
-            itemCount: data!.length,
-            itemBuilder: (BuildContext, index) {
-              return Slidable(
-                key: ValueKey(index),
-                actionPane: SlidableDrawerActionPane(),
-                secondaryActions: <Widget>[
-                  IconSlideAction(
-                      caption: 'Delete',
-                      color: Colors.red.shade300,
-                      icon: Icons.remove_circle,
-                      closeOnTap: true,
-                      onTap: () {
-                        Fluttertoast.showToast(
-                          msg: 'Delete on ${index}',
-                          toastLength: Toast.LENGTH_SHORT,
-                          gravity: ToastGravity.BOTTOM,
-                        );
-                      })
-                ],
-                child: ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: NetworkImage(data![index].picture),
-                  ),
-                  title: Text(
-                    data![index].album,
-                  ),
-                  subtitle: Text(
-                    'By ${data![index].name}',
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      new MaterialPageRoute(
-                        builder: (context) => DetailPage(data![index]),
-                      ),
-                    );
-                  },
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: data == null
+          ? Container(
+              child: Center(
+                child: LoadingBouncingGrid.square(
+                  backgroundColor: Colors.red,
+                  borderColor: Colors.red,
+                  duration: const Duration(seconds: 3),
                 ),
-              );
-            },
-          ),
-        ),
-      );
-    }
+              ),
+            )
+          : Container(
+              child: ListView.builder(
+                itemCount: data!.length,
+                itemBuilder: (BuildContext, index) {
+                  return Dismissible(
+                    key: new Key(data![index].album),
+                    confirmDismiss: (direction) async {
+                      return await showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: const Text("Delete Confirmation"),
+                            content: const Text(
+                                "Are you sure you want to delete this item?"),
+                            actions: <Widget>[
+                              TextButton(
+                                  onPressed: () =>
+                                      Navigator.of(context).pop(true),
+                                  child: const Text("Delete")),
+                              TextButton(
+                                onPressed: () =>
+                                    Navigator.of(context).pop(false),
+                                child: const Text("Cancel"),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    onDismissed: (direction) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(
+                              "Song \"${data![index].album}\" was removed."),
+                        ),
+                      );
+                      setState(() {
+                        data!.removeAt(index);
+                        print(json.encode(data));
+                      });
+                    },
+                    background: new Container(
+                      color: Colors.red.shade300,
+                    ),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(data![index].picture),
+                      ),
+                      title: Text(
+                        data![index].album,
+                      ),
+                      subtitle: Text(
+                        'By ${data![index].name}',
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          new MaterialPageRoute(
+                            builder: (context) => DetailPage(data![index]),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                },
+              ),
+            ),
+    );
   }
 }
 
@@ -164,4 +169,21 @@ class User {
     this.album,
     this.picture,
   );
+
+  factory User.fromJson(dynamic json) {
+    return User(
+      json['index'] as int,
+      json['name'] as String,
+      json['album'] as String,
+      json['picture'] as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() => 
+  {
+    'index': index,
+    'name': name,
+    'album': album,
+    'picture': picture,
+  };
 }
