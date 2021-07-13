65c163845ace765c49eb59ecf34f7a783f3f1a06
MinhTriNguyen <minhtri.minhzzxzx@gmail.com>
2021-07-13 22:27:12 +0700

    Add scrollbar

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:loading_animations/loading_animations.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'dart:convert';
+import 'dart:io';
 
 import './user.dart';
 import './listdetail.dart';
@@ -17,9 +19,19 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<User>? data;
+  File? jsonFile;
+  Directory? dir;
+  String fileName = "list_data.json";
+  bool fileExists = false;
+
+  TextEditingController songNameController = TextEditingController();
+  TextEditingController authorNameController = TextEditingController();
+  bool isSongNameValidate = true;
+  bool isAuthorNameValidate = true;
+
   Future<String> getJson() => rootBundle.loadString('assets/data.json');
 
-  void _getUsers() async {
+  void _getSongList() async {
     var _data = json.decode(await getJson()) as List;
     List<User> users = _data.map((user) => User.fromJson(user)).toList();
 
@@ -29,23 +41,42 @@ class _MyHomePageState extends State<MyHomePage> {
     print('done fetching');
   }
 
-  void _addUser(String name, String album) {
+  void _addSong(String name, String album) {
     String picture = 'http://lorempixel.com/400/400/people';
     setState(() {
       data!.insert(data!.length, User(name, album, picture));
     });
+    _writeToFile();
   }
 
-  void _writeJson(int index) {
+  void _deleteSong(int index) {
     setState(() => data!.removeAt(index));
+    _writeToFile();
   }
 
-  TextEditingController songNameController = TextEditingController();
-  TextEditingController authorNameController = TextEditingController();
-  bool isSongNameValidate = true;
-  bool isAuthorNameValidate = true;
+  void _createFile(Directory dir, String fileName) {
+    print("Creating file!");
+    File file = new File(dir.path + "/" + fileName);
+    file.createSync();
+    fileExists = true;
+    file.writeAsStringSync(json.encode(data));
+  }
 
-  bool validateTextField(String userInput) {
+  void _writeToFile() {
+    print("Writing to file!");
+
+    if (fileExists) {
+      print("File exists");
+      jsonFile!.writeAsStringSync(json.encode(data));
+    } else {
+      print("File does not exist!");
+      _createFile(dir!, fileName);
+    }
+    //this.setState(() => _getSongList());
+    print(data![data!.length - 1].album);
+  }
+
+  bool _validateText(String userInput) {
     setState(
       () => {
         if (songNameController.text.isEmpty) isSongNameValidate = false,
@@ -56,75 +87,79 @@ class _MyHomePageState extends State<MyHomePage> {
     return userInput.isNotEmpty;
   }
 
-  void _showDialog() async {
-    await showDialog<String>(
-      context: context,
-      builder: (context) => AlertDialog(
-        contentPadding: const EdgeInsets.all(16.0),
-        content: Container(
-          height: 180,
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                child: TextField(
-                  controller: songNameController,
-                  autofocus: true,
-                  decoration: InputDecoration(
-                    labelText: 'Song Name',
-                    hintText: 'eg. Never Gonna Give You Up',
-                    errorText:
-                        isSongNameValidate ? null : 'Please enter a song name.',
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextField(
-                  controller: authorNameController,
-                  decoration: InputDecoration(
-                    labelText: 'Author',
-                    hintText: 'eg. Rick Astley',
-                    errorText: isAuthorNameValidate
-                        ? null
-                        : 'Please enter author name.',
-                  ),
-                ),
-              ),
-            ],
+  void _showDialog() async => await showDialog<String>(
+        context: context,
+        builder: (context) => AlertDialog(
+          contentPadding: const EdgeInsets.all(16.0),
+          content:
+              Container(height: 180, child: Column(children: _dialogSongAdd())),
+          actions: _dialogSongConfirm(context),
+        ),
+      );
+
+  List<Widget> _dialogSongAdd() {
+    return <Widget>[
+      Expanded(
+        child: TextField(
+          controller: songNameController,
+          autofocus: true,
+          decoration: InputDecoration(
+            labelText: 'Song Name',
+            hintText: 'eg. Never Gonna Give You Up',
+            errorText: isSongNameValidate ? null : 'Please enter a song name.',
           ),
         ),
-        actions: <Widget>[
-          TextButton(
-            child: const Text('SAVE'),
-            onPressed: () => {
-              isSongNameValidate = validateTextField(songNameController.text),
-              isAuthorNameValidate =
-                  validateTextField(authorNameController.text),
-              if (isSongNameValidate && isAuthorNameValidate)
-                {
-                  _addUser(authorNameController.text, songNameController.text),
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text(
-                          "Song \"${songNameController.text}\" by \"${authorNameController.text}\" was added."),
-                    ),
-                  ),
-                  Navigator.pop(context)
-                }
-            },
-          ),
-          TextButton(
-            child: const Text('CANCLE'),
-            onPressed: () => Navigator.pop(context),
-          ),
-        ],
       ),
-    );
+      Expanded(
+        child: TextField(
+          controller: authorNameController,
+          decoration: InputDecoration(
+            labelText: 'Author',
+            hintText: 'eg. Rick Astley',
+            errorText:
+                isAuthorNameValidate ? null : 'Please enter author name.',
+          ),
+        ),
+      ),
+    ];
+  }
+
+  List<Widget> _dialogSongConfirm(BuildContext context) {
+    return <Widget>[
+      TextButton(
+        child: const Text('SAVE'),
+        onPressed: () => {
+          isSongNameValidate = _validateText(songNameController.text),
+          isAuthorNameValidate = _validateText(authorNameController.text),
+          if (isSongNameValidate && isAuthorNameValidate)
+            {
+              _addSong(authorNameController.text, songNameController.text),
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                      "Song \"${songNameController.text}\" by \"${authorNameController.text}\" was added."),
+                ),
+              ),
+              Navigator.pop(context)
+            }
+        },
+      ),
+      TextButton(
+        child: const Text('CANCLE'),
+        onPressed: () => Navigator.pop(context),
+      ),
+    ];
   }
 
   @override
   void initState() {
     super.initState();
-    _getUsers();
+    getApplicationDocumentsDirectory().then((Directory directory) {
+      dir = directory;
+      jsonFile = new File(dir!.path + "/" + fileName);
+      fileExists = jsonFile!.existsSync();
+      _getSongList();
+    });
   }
 
   @override
@@ -134,82 +169,125 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: data == null
-          ? Container(
-              child: Center(
-                child: LoadingBouncingGrid.square(
-                  backgroundColor: Colors.red,
-                  borderColor: Colors.red,
-                  duration: const Duration(seconds: 3),
-                ),
-              ),
-            )
-          : Container(
-              child: ListView.separated(
-                itemCount: data!.length,
-                separatorBuilder: (context, int index) => const Divider(
-                    height: 2, thickness: 0.5, indent: 15, endIndent: 15),
-                itemBuilder: (context, index) {
-                  return Dismissible(
-                    key: Key(data![index].album),
-                    direction: DismissDirection.endToStart,
-                    confirmDismiss: (direction) async {
-                      return await showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            title: const Text("Delete Confirmation"),
-                            content: const Text(
-                                "Are you sure you want to delete this item?"),
-                            actions: <Widget>[
-                              TextButton(
-                                  onPressed: () =>
-                                      Navigator.of(context).pop(true),
-                                  child: const Text("DELETE")),
-                              TextButton(
-                                onPressed: () =>
-                                    Navigator.of(context).pop(false),
-                                child: const Text("CANCEL"),
-                              ),
-                            ],
-                          );
-                        },
-                      );
-                    },
-                    onDismissed: (direction) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text(
-                              "Song \"${data![index].album}\" was removed."),
-                        ),
-                      );
-                      _writeJson(index);
-                    },
-                    background: Container(
-                      padding: EdgeInsets.symmetric(horizontal: 20),
-                      alignment: AlignmentDirectional.centerEnd,
-                      child: Icon(Icons.delete, color: Colors.white),
-                      color: Colors.red.shade400,
+          ? _loadScreen()
+          : Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  buildButton(
+                    text: 'Show my favourite play list',
+                    onClicked: () => showModalBottomSheet(
+                      isScrollControlled: true,
+                      backgroundColor: Colors.transparent,
+                      context: context,
+                      builder: (context) => buildSheet(),
                     ),
-                    child: ListTile(
-                      leading: CircleAvatar(
-                          backgroundImage: NetworkImage(data![index].picture)),
-                      title: Text(data![index].album),
-                      subtitle: Text('By ${data![index].name}'),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => DetailPage(data![index])),
-                        );
-                      },
-                    ),
-                  );
-                },
+                  ),
+                ],
               ),
             ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () => _showDialog(),
+      ),
+    );
+  }
+
+  Widget buildButton({required String text, required VoidCallback onClicked}) =>
+      ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          shape: StadiumBorder(),
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+        ),
+        child: Text(text, style: TextStyle(fontSize: 20)),
+        onPressed: onClicked,
+      );
+
+  Widget buildSheet() => DraggableScrollableSheet(
+        initialChildSize: 0.85,
+        minChildSize: 0.84,
+        maxChildSize: 0.85,
+        builder: (_, controller) => _listBuild(controller),
+      );
+
+  Container _loadScreen() {
+    return Container(
+      child: Center(
+        child: LoadingBouncingGrid.square(
+          backgroundColor: Colors.red,
+          borderColor: Colors.red,
+          duration: const Duration(seconds: 3),
+        ),
+      ),
+    );
+  }
+
+  Container _listBuild(ScrollController controller) {
+    return Container(
+      padding: EdgeInsets.only(top: 20, left: 10, right: 10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      child: ListView.separated(
+        controller: controller,
+        itemCount: data!.length,
+        separatorBuilder: (context, int index) =>
+            const Divider(height: 2, thickness: 0.5, indent: 15, endIndent: 15),
+        itemBuilder: _songBuild,
+      ),
+    );
+  }
+
+  Widget _songBuild(context, index) {
+    return Dismissible(
+      key: Key(data![index].album),
+      direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) async {
+        return await showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: const Text("Delete Confirmation"),
+              content: const Text("Are you sure you want to delete this item?"),
+              actions: <Widget>[
+                TextButton(
+                    onPressed: () => Navigator.of(context).pop(true),
+                    child: const Text("DELETE")),
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(false),
+                  child: const Text("CANCEL"),
+                ),
+              ],
+            );
+          },
+        );
+      },
+      onDismissed: (direction) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Song \"${data![index].album}\" was removed."),
+          ),
+        );
+        _deleteSong(index);
+      },
+      background: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        alignment: AlignmentDirectional.centerEnd,
+        child: Icon(Icons.delete, color: Colors.white),
+        color: Colors.red.shade400,
+      ),
+      child: ListTile(
+        leading:
+            CircleAvatar(backgroundImage: NetworkImage(data![index].picture)),
+        title: Text(data![index].album),
+        subtitle: Text('By ${data![index].name}'),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => DetailPage(data![index])),
+          );
+        },
       ),
     );
   }
